705847c10a7fc9b28606657be5db0f9d5fc795ac
Stan <StanBPublic@live.com>
2014-12-01 13:40:53 -0500

    Form is done

--- CodeQualityPortal/Scripts/App/churn/churn-controller.js
+++ CodeQualityPortal/Scripts/App/churn/churn-controller.js
@@ -1,7 +1,21 @@
-﻿/// <reference path="churn-controller.js" />
-churnModule.controller("ChurnController", function ($scope, bootstrappedData) {
-    $scope.repoOptions = bootstrappedData.repoOptions;
-    if ($scope.repoOptions.length > 0) {
-        $scope.repo = $scope.repoOptions[0].repoId;
+﻿'use strict';
+
+/// <reference path="churn-controller.js" />
+
+churnModule.controller("ChurnController", function ($scope, bootstrappedData) {    
+    var dateFrom = new Date();
+    dateFrom.setDate(dateFrom.getDate() - 14);
+    $scope.criteria = {
+        repoOptions: bootstrappedData.repoOptions,
+        dateFrom: dateFrom,
+        dateTo: new Date(),
+        extension:"cs"
+    };
+    if ($scope.criteria.repoOptions.length > 0) {
+        $scope.criteria.repo = $scope.criteria.repoOptions[0];
+    }
+
+    $scope.refresh = function(criteria) {
+        console.log(criteria);
     }
 });
