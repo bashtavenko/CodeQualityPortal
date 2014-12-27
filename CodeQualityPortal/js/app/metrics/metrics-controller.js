edbfc6400930b517e241e54a5c882e8c6d624659
Stan <StanBPublic@live.com>
2014-12-27 12:03:55 -0500

    Nice refactoring of service

--- CodeQualityPortal/js/app/metrics/metrics-controller.js
+++ CodeQualityPortal/js/app/metrics/metrics-controller.js
@@ -1,49 +1,34 @@
 ﻿'use strict';
 
-metricsModule.controller("MetricsController", function ($scope, bootstrappedData, $resource, $log) {    
+metricsModule.controller("MetricsController", function ($scope, bootstrappedData, $resource, $log, metricsService) {    
     $scope.criteria = {}; // shared with child controllers   
                     
     $scope.init = function () {        
         $scope.refreshChart();
     }    
 
-    $scope.refreshChart = function () {        
-        var params = {
-            tag: $scope.criteria.tag,
-            dateFrom: $scope.criteria.dateFrom.toISOString().slice(0, 10).replace(/-/g, '-'),
-            dateTo: $scope.criteria.dateTo.toISOString().slice(0, 10).replace(/-/g, '-')
-        };
-                
-        var Trend = $resource("/api/moduletrend/:tag/:dateFrom/:dateTo",
-            { tag: "@tag", dateFrom: "@dateFrom", dateTo: "@dateTo" });
-        
-        Trend.query(params,
-            function (data) {
-                $scope.criteria.selectedDate = null;
-                $scope.items = [];
-                $scope.$broadcast("adjust_chart");
-                $scope.trendData = data;
-            },
-            function (error) {
-                $log.error(error);
-            });        
+    $scope.refreshChart = function () {
+        metricsService.getTagTrend($scope.criteria.tag, $scope.criteria.dateFrom, $scope.criteria.dateTo)
+            .$promise.then(
+                function(data) {
+                    $scope.criteria.selectedDate = null;
+                    $scope.items = [];
+                    $scope.$broadcast("adjust_chart");
+                    $scope.trendData = data;
+                },
+                function (error) {
+                    $log.error(error);
+                });
     }
 
     $scope.refreshGrid = function () {
-        var params = {
-            tag: $scope.criteria.tag,
-            dateId: $scope.criteria.selectedDate.dateId
-        };
-
-        var Modules = $resource("/api/modules/:tag/:dateId",
-            { tag: "@tag", dateId: "@dateId"});
-        Modules.query(params,
-            function (data) {
-                $scope.items = data
-            },
-            function (error) {
-                $log.error(error);
-            });
+        // Can't bind directly to items since wijmo doesn't like promise
+        metricsService.getModulesByDate($scope.criteria.tag, $scope.criteria.selectedDate.dateId)
+            .$promise.then(
+                function (data) {
+                    $scope.items = data;
+                }
+        );
     }
 });
 
