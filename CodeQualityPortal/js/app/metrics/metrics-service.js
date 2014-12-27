d6bc11b1adf8cbdaf7151cf10e333ccab431ee20
Stan <StanBPublic@live.com>
2014-12-27 13:19:13 -0500

    Missing file

--- CodeQualityPortal/js/app/metrics/metrics-service.js
+++ CodeQualityPortal/js/app/metrics/metrics-service.js
@@ -6,7 +6,27 @@ metricsModule.factory("metricsService", function($resource) {
             var params = {
                 tag: tag                
             };
-            return $resource("/api/modules/:tag", { tag: "@tag" }).query(params);
+            return $resource("/api/modules/:tag", { tag: "@tag" })
+                .query(params);
+        },
+        getTagTrend: function (tag, dateFrom, dateTo) {
+            var params = {
+                tag: tag,
+                dateFrom: dateFrom.toISOString().slice(0, 10).replace(/-/g, '-'),
+                dateTo: dateTo.toISOString().slice(0, 10).replace(/-/g, '-')
+            };
+
+            return $resource("/api/moduletrend/:tag/:dateFrom/:dateTo",
+                { tag: "@tag", dateFrom: "@dateFrom", dateTo: "@dateTo" })
+                .query(params);
+        },
+        getModulesByDate: function (tag, dateId) {
+            var params = {
+                tag: tag,
+                dateId: dateId
+            };
+            return $resource("/api/modules/:tag/:dateId", { tag: "@tag", dateId: "@dateId" })
+                .query(params);
         }
     };
 });
