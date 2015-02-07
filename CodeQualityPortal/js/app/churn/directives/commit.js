b0fa7980c73001dbcfad0079281f96661c2d17c4
Stan <StanBPublic@live.com>
2015-02-07 12:30:13 -0500

    Commit directive fixed

--- CodeQualityPortal/js/app/churn/directives/commit.js
+++ CodeQualityPortal/js/app/churn/directives/commit.js
@@ -7,7 +7,12 @@ churnModule.directive('commit', function () {
         templateUrl: '/js/app/churn/templates/commit.html',
         scope: {
             commit: '=',
-            method: '&'
+            method: '&' // method should not have parenthesis
+        },
+        controller: function($scope) {
+            $scope.innerCommitClick = function() {
+                $scope.method()($scope.commit);
+            }
         }
     }
 });
