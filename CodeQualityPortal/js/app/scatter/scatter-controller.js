1907a5446168a097189bbec8ccf13ebebc66a425
Stan <StanBPublic@live.com>
2015-09-23 10:26:57 -0400

    Improved rendering of outliers on bubble chart.

--- CodeQualityPortal/js/app/scatter/scatter-controller.js
+++ CodeQualityPortal/js/app/scatter/scatter-controller.js
@@ -7,10 +7,20 @@ scatterModule.controller("ScatterController", function ($scope, bootstrappedData
 
     $scope.dates = bootstrappedData.dates;    
     $scope.cdi = $scope.dates.length - 1;
+    $scope.minCodeCoverage = 100;
+    $scope.maxLinesOfCode = 0;
     
     $scope.getDate = function () {        
         return $scope.dates[$scope.cdi];
     };
+
+    $scope.getMaxLinesOfCode = function () {
+        return $scope.maxLinesOfCode + $scope.maxLinesOfCode * 0.03;
+    }
+
+    $scope.getMinCodeCoverage = function () {
+        return $scope.minCodeCoverage == 0 ? -7 : $scope.minCodeCoverage;
+    }
 
     $scope.goForward = function () {
         $scope.cdi = $scope.cdi + 1;
@@ -42,8 +52,15 @@ scatterModule.controller("ScatterController", function ($scope, bootstrappedData
             .$promise.then(
                 function (data) {
                     // Wijmo can't bind series to 'name' property
+                    // We also want to adjust chart to render outliers better                    
                     for (var i = 0; i < data.length; i++) {
                         data[i].systemName = data[i].name;
+                        if ($scope.minCodeCoverage > data[i].codeCoverage) {
+                            $scope.minCodeCoverage = data[i].codeCoverage;
+                        }
+                        if ($scope.maxLinesOfCode < data[i].linesOfCode) {
+                            $scope.maxLinesOfCode = data[i].linesOfCode;
+                        }
                     }
                     $scope.scatterData = data;                    
                 }
