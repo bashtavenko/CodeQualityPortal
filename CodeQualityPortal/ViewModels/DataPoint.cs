fbd9e8e875a2a35d76aab716ee91921bdda91ddd
Stan <StanBPublic@live.com>
2015-10-10 10:23:29 -0400

    Added trends back on. Fixed data point bug.

--- CodeQualityPortal/ViewModels/DataPoint.cs
+++ CodeQualityPortal/ViewModels/DataPoint.cs
@@ -7,5 +7,6 @@ namespace CodeQualityPortal.ViewModels
         public int DateId { get; set; }
         public DateTime Date { get; set; }        
         public string DateString { get { return Date.ToString("MM/dd"); } }
+        public int Value { get; set; }
     }
 }
